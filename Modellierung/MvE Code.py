18187751282484408fd9cd8208a9423dfec63ff1
jonathanthkoeln <jonathan.baumann@smail.th-koeln.de>
2026-06-01 14:52:16 +0200

    Create MvE Code.py

---
+++ Modellierung/MvE Code.py
@@ -0,0 +1,12 @@
+#%% Bibliotheken importieren
+
+import pypsa
+import pandas as pd
+import numpy as np
+
+#%% Datenimport
+
+dynamischer_strompreis = pd.read_csv("Strompreis.csv", sep=';', decimal=',')
+# %%
+dynamischer_strompreis["Strompreis dyn. 2040 SE"].plot()
+# %%
