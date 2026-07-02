a1c9f270d3dba3eebb0d5bad4409b15a89b4d619
jonathanthkoeln <jonathan.baumann@smail.th-koeln.de>
2026-07-02 18:04:23 +0200

    Update MvE Code.py

--- MvE Code.py
+++ MvE Code.py
@@ -48,7 +48,7 @@ network.add("Bus", name = "E-Bus")
 #network.add("Bus", name = "BS")
 
 network.add("Generator", name = "Stromnetz", bus = "Electricity", p_nom = 10000, marginal_cost = dynamischer_strompreis)
-network.add("Generator", name = "PV", bus = "Electricity", p_nom_extendable = True, p_max_pu = einstrahlung_süd["PV Leistung in kW"])
+network.add("Generator", name = "PV", bus = "Electricity", p_nom_extendable = True, p_max_pu = einstrahlung_süd["PV Leistung in kW"], capital_cost = cost_pv)
 network.add("Generator", name = "Einspeisung", bus = "Electricity", p_nom = 10000, sign = -1, marginal_cost = einspeisevergütung)
 
 network.add("Store", name = "BS stationär", bus = "Electricity", e_nom_extendable = True, e_nom_max = 10000, capital_cost = cost_bs)
@@ -56,7 +56,7 @@ network.add("Store", name = "BS stationär", bus = "Electricity", e_nom_extendab
 network.add("Store", name = "E-Bus 1", bus = "E-Bus", e_nom = e_nom_ebus)   #Kosten weglassen? (Die Entscheidung wurde ja quasi getroffen,
                                                                             #dass solche E-Busse vorhanden sein sollen, daher ggf. Kosten nicht relevant)
 network.add("Load", name = "Last Standort", bus = "Electricity", p_set = lastprofil_standort)
-network.add("Load", name = "Last Standort", bus = "Electricity", p_set = lastprofil_ebus)
+network.add("Load", name = "Last E-Bus", bus = "Electricity", p_set = lastprofil_ebus)
 
 network.add("Link", name = "E-Bus laden", bus0 = "Electricity", bus1 = "E-Bus", p_nom_max = 10000, efficiency = effizienz_ebus_laden)
 network.add("Link", name = "E-Bus entladen", bus0 = "E-Bus", bus1 = "Electricity", p_nom_max = 10000, efficiency = effizienz_ebus_entladen)
