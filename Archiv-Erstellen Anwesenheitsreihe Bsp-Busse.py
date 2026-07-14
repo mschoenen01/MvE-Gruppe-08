9535067cfc24611f4e535ce43ce5fd4c92c87daa
jonathanthkoeln <jonathan.baumann@smail.th-koeln.de>
2026-07-14 18:28:50 +0200

    Create Archiv-Erstellen Anwesenheitsreihe Bsp-Busse.py

---
+++ Archiv-Erstellen Anwesenheitsreihe Bsp-Busse.py
@@ -0,0 +1,32 @@
+#%% Bibliotheken importieren
+
+import pandas as pd
+import csv
+
+#%% df Import
+
+df = pd.read_csv("Fahrzeiten 3 Busse Bsp neu.CSV", sep=";", dtype=int)
+
+bus1_abfahrt = df.iloc[0, 0]
+bus1_ankommen = df.iloc[0, 1]
+bus2_abfahrt = df.iloc[0, 2]
+bus2_ankommen = df.iloc[0, 3]
+bus3_abfahrt = df.iloc[0, 4]
+bus3_ankommen = df.iloc[0, 5]
+
+#%% Schleife für Hinzufügen von Spalten mit 0 und 1
+
+with open("Fahrzeiten 3 Busse Bsp.CSV", mode="w", newline="", encoding="utf-8") as file:
+    writer = csv.writer(file)
+
+    writer.writerow(["Snapshot", "Ladeleistung Bus1", "Ladeleistung Bus2", "Ladeleistung Bus3"])
+
+    for i in range(96):
+
+        ladezustand_bus1 = 0 if i >= bus1_abfahrt and i < bus1_ankommen else 1
+
+        ladezustand_bus2 = 0 if i >= bus2_abfahrt and i < bus2_ankommen else 1
+
+        ladezustand_bus3 = 0 if i >= bus3_abfahrt and i < bus3_ankommen else 1
+
+        writer.writerow([i, ladezustand_bus1, ladezustand_bus2, ladezustand_bus3])
