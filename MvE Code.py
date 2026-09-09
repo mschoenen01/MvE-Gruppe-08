11a53e6607d088aba7ec0aa888a2ff80a2c3563e
jonathanthkoeln <jonathan.baumann@smail.th-koeln.de>
2026-09-09 08:19:35 +0200

    Update MvE Code.py

--- MvE Code.py
+++ MvE Code.py
@@ -103,7 +103,7 @@ network.add("Bus", name = "BS")
 
 #++++++++++ Generatoren ++++++++++
 
-network.add("Generator", name = "Stromnetz", bus = "Electricity", p_nom = 10000, marginal_cost = dynamischer_strompreis)
+network.add("Generator", name = "Stromnetz", bus = "Electricity", p_nom = 10000, marginal_cost = strompreis_statisch)
 network.add("Generator", name = "PV", bus = "Electricity", p_nom_extendable = True, p_nom_max = 290, p_max_pu = einstrahlung_süd["PV Leistung in kW"].values, capital_cost = fixkosten_pv_jährlich)
 network.add("Generator", name = "Einspeisung", bus = "Electricity", p_nom = 10000, sign = -1, marginal_cost = einspeisevergütung)
 network.add("Generator", name = "PV Carport West", bus = "Electricity", p_nom_extendable = True, p_nom_max = 5000, p_max_pu = einstrahlung_west["PV Leistung in kW"].values, capital_cost = fixkosten_pv_carport_jährlich)
